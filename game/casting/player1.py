5d4fe352c9acc3a1db5d872a44fa2f43d1a76702
apache-pine <kylar.sorensen@gmail.com>
2022-11-10 17:45:37 -0800

    updated player classes

--- game/casting/player1.py
+++ game/casting/player1.py
@@ -18,6 +18,8 @@ class Player2(Cycle):
         for i in range(constants.SNAKE_LENGTH):
             position = Point(x - i * constants.CELL_SIZE, y)
             velocity = Point(1, 0)
+            text = "8" if i == 0 else "#"
+            color = constants.YELLOW if i == 0 else constants.GREEN
 
             segment = Actor()
             segment.set_position(position)
