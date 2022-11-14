2e7de1ac2a52529c8f099fc607010918d4f01825
mckayythomas <mckayycollins@gmail.com>
2022-11-13 20:13:30 -0500

    changed all ocurances of snake to cylce

--- game/scripting/control_actors_action.py
+++ game/scripting/control_actors_action.py
@@ -20,8 +20,7 @@ class ControlActorsAction(Action):
             keyboard_service (KeyboardService): An instance of KeyboardService.
         """
         self._keyboard_service = keyboard_service
-        self._direction1 = Point(constants.CELL_SIZE, 0)
-        self._direction2 = Point(constants.CELL_SIZE, 0)
+        self._direction = Point(constants.CELL_SIZE, 0)
 
 
     def execute(self, cast, script):
@@ -33,38 +32,20 @@ class ControlActorsAction(Action):
         """
         # left
         if self._keyboard_service.is_key_down('a'):
-            self._direction1 = Point(-constants.CELL_SIZE, 0)
+            self._direction = Point(-constants.CELL_SIZE, 0)
         
         # right
         if self._keyboard_service.is_key_down('d'):
-            self._direction1 = Point(constants.CELL_SIZE, 0)
+            self._direction = Point(constants.CELL_SIZE, 0)
         
         # up
         if self._keyboard_service.is_key_down('w'):
-            self._direction1 = Point(0, -constants.CELL_SIZE)
+            self._direction = Point(0, -constants.CELL_SIZE)
         
         # down
         if self._keyboard_service.is_key_down('s'):
-            self._direction1 = Point(0, constants.CELL_SIZE)
+            self._direction = Point(0, constants.CELL_SIZE)
         
         cycle = cast.get_first_actor("cycles")
-        cycle.turn_head(self._direction1)
-
-                # left
-        if self._keyboard_service.is_key_down('j'):
-            self._direction2 = Point(-constants.CELL_SIZE, 0)
-        
-        # right
-        if self._keyboard_service.is_key_down('l'):
-            self._direction2 = Point(constants.CELL_SIZE, 0)
-        
-        # up
-        if self._keyboard_service.is_key_down('i'):
-            self._direction2 = Point(0, -constants.CELL_SIZE)
-        
-        # down
-        if self._keyboard_service.is_key_down('k'):
-            self._direction2 = Point(0, constants.CELL_SIZE)
-        
-        cycle = cast.get_second_actor("cycles")
-        cycle.turn_head(self._direction2)
+        cycle.turn_head(self._direction)
+    
