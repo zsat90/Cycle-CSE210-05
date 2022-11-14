e5df0ed5fbc59bb2b149db1dc43efe05b614e131
Zakery Sattler <zakerysattler@Zakerys-MacBook-Pro-5.local>
2022-11-13 17:03:05 -0700

    added logic for tail growth when moving

--- game/casting/player1.py
+++ game/casting/player1.py
@@ -11,14 +11,27 @@ class Player1(Cycle):
         self._segments = []
         self._prepare_body()
     #polyhere
+    def grow_tail(self):
+        tail = self._segments[-1]
+        velocity = tail.get_velocity()
+        offset = velocity.reverse()
+        position = tail.get_position().add(offset)
+
+        segment = Actor()
+        segment.set_position(position)
+        segment.set_velocity(velocity)
+        segment.set_text("#")
+        segment.set_color(constants.GREEN)
+        self._segments.append(segment)
+
     def _prepare_body(self):
         x = int(constants.MAX_X / .25)
         y = int(constants.MAX_Y / 2)
 
-        for i in range(constants.SNAKE_LENGTH):
+        for i in range(constants.CYCLE_LENGTH):
             position = Point(x - i * constants.CELL_SIZE, y)
             velocity = Point(1 * constants.CELL_SIZE, 0)
-            text = "8" if i == 0 else "#"
+            text = "0" if i == 0 else "#"
             color = constants.YELLOW if i == 0 else constants.RED
 
             segment = Actor()
@@ -27,18 +40,6 @@ class Player1(Cycle):
             segment.set_text(text)
             segment.set_color(color)
             self._segments.append(segment)
-    #polyhere
-    def grow_tail(self, number_of_segments):
-        for i in range(number_of_segments):
-            tail = self._segments[-1]
-            velocity = tail.get_velocity()
-            offset = velocity.reverse()
-            position = tail.get_position().add(offset)
-
-            segment = Actor()
-            segment.set_position(position)
-            segment.set_velocity(velocity)
-            segment.set_text("#")
-            segment.set_color(constants.GREEN)
-            self._segments.append(segment)
+   
+   
 
